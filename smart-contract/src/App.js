d0e32646707e399e553f35fb1957ee98ebbbb170
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-11 14:07:10 +0300

    check for wallet

--- smart-contract/src/App.js
+++ smart-contract/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 
 import { init, useConnectWallet } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
-import { ethers } from "ethers";
+import { errors, ethers } from "ethers";
+import ChainModal from './components/ChainModal/ChainModal';
 
 const API_KEY = "muZQr36vDyKnI9BAn76g4J10E9i3oNbn";
 const rpcUrl = `https://eth-sepolia.g.alchemy.com/v2/${API_KEY}`;
@@ -28,7 +29,32 @@ init({
 function App() {
   const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
 
+  function handleDisconnect() {
+    if (!wallet) {
+      return;
+    }
 
+    disconnect(wallet).catch((error) => {
+      console.error(error);
+    });
+  }
+
+
+  if(wallet){
+    return (
+      <div className="App">
+        <ChainModal />
+        <button
+          disabled={connecting}
+          onClick={() => (wallet ? disconnect(wallet) : connect())}
+          >
+          {connecting ? "connecting" : wallet ? "disconnect" : "connect"}
+        </button>
+
+      </div>
+    );
+
+  }
   return (
     <div className="App">
       <button
